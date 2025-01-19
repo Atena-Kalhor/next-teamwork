c197bb3c3021e826c241c2589edb64bfb8b22ed6
FatemehSafaee <fa.safa135@gmail.com>
2025-01-19 19:34:56 +0330

    push question cards

--- src/app/question/page.jsx
+++ src/app/question/page.jsx
@@ -1,1 +1,13 @@
+import Qcards from "@/components/Qcards";
 import Searchbar from "@/components/Searchbar";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Qcards />
+    </div>
+  );
+}
+
+export default page;
